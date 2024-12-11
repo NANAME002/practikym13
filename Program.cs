f522d27f225636f7f80e8014b0c60a42da7d3e3a
NANAME002 <dennzerogo@gmail.com>
2024-12-11 10:41:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = Input("предмет= ");
+            Console.WriteLine($"{GetSubject(subject)}");
+            Console.ReadLine();
+        }
+        static string Input(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        static string GetSubject(string subject)
+        {
+            switch(subject)
+            {
+                case "ф": return "физика";
+                case "м": return "математика";
+                case "и": return "история";
+                case "г": return "география";
+                default:return "Ошибка";
+            }
+        }
+    }
+}
